c86b750763f103f8c19d3c5e006b8e6845c81b9c
matsumoto <matzz0219@gmail.com>
2026-03-15 11:42:49 +0900

    施設登録機能を実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,45 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  # READ
   def index
   end
 
-  def new
-  end
-
-  def create
-  end
-
+  # READ
   def show
+  end
+
+  # CREATE
+  def new
+    @room = Room.new
+  end
+
+  # CREATE
+  def create
+    @room = current_user.rooms.build(room_params)
+
+    if @room.save
+      redirect_to rooms_path, notice: "施設を登録しました"
+    else
+      render :new
+    end
+  end
+
+  # UPDATE
+  def edit
+  end
+
+  # UPDATE
+  def update
+  end
+
+  # DELETE
+  def destroy
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description, :price, :address, :image_name)
   end
 end
