d9fb61cf4dcfb21b26c7e27f5d129a9a0f3f9733
matsumoto <matzz0219@gmail.com>
2026-03-15 11:22:32 +0900

    roomsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "rooms/index"
+  get "rooms/new"
+  get "rooms/create"
+  get "rooms/show"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
