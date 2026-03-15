cb94179ef1bfeaeccd7172d0a2a099d0d7b77183
matsumoto <matzz0219@gmail.com>
2026-03-15 10:25:23 +0900

    roomモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_15_003852) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_15_012443) do
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.string "address"
+    t.string "image_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_15_003852) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "rooms", "users"
 end
