36d90e1c530e1f1e4485695c8e710c9b6ca14a7b
matsumoto <matzz0219@gmail.com>
2026-03-15 10:32:02 +0900

    roomモデルのリレーション、バリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,9 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true                                                  # 施設名は必須
+  validates :description, presence: true                                           # 施設詳細は必須
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }  # 宿泊料金は必須、かつ1円以上
+  validates :address, presence: true                                               # 住所は必須
 end
