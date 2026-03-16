6c4327ed25db783e86846466653759e56120370b
matsumoto <matzz0219@gmail.com>
2026-03-16 22:42:05 +0900

    reservationモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_15_012443) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_16_134109) do
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.date "check_in"
+    t.date "check_out"
+    t.integer "people"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_reservations_on_room_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name"
@@ -38,5 +51,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_15_012443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
   add_foreign_key "rooms", "users"
 end
