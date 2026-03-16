acd7c4f1df9edbd871a89c61da67e36bc689d404
matsumoto <matzz0219@gmail.com>
2026-03-16 23:11:19 +0900

    予約機能の土台を実装

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,35 @@
 class ReservationsController < ApplicationController
+  # CREATE
   def new
+    @room = Room.find(params[:room_id])
+    @reservation = Reservation.new
   end
 
+  # CREATE
   def create
+    @room = Room.find(reservation_params[:room_id])
+    @reservation = current_user.reservations.build(reservation_params)
+
+    if @reservation.save
+      redirect_to reservations_path, notice: "予約しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  # READ
   def index
+    @reservations = current_user.reservations.includes(:room)
   end
 
+  # READ
   def show
+    @reservation = current_user.reservations.find(params[:id])
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:room_id, :check_in, :check_out, :people, :total_price)
   end
 end
